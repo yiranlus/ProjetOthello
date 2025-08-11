272951e76cf4eacc23a84b6afda5de7e8e15c7b4
Yiran Lu <yiranls@outlook.com>
2025-08-11 16:02:24 +0200

    update Case.py

---
+++ src/othello/Case.py
@@ -0,0 +1,14 @@
+class Case:
+    def __init__(self, pawn=None):
+        self._pawn = pawn
+
+    @property
+    def pawn(self):
+        return self._pawn
+
+    @pawn.setter
+    def pawn(self, value):
+        self._pawn = value
+
+    def is_empty(self):
+        return self._pawn is None
