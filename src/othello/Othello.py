58c2e77cdaf9ca7859ee6933d17165a098a3b786
Mehdi Chouchane <mehdi.chouchan@gmail.com>
2025-08-11 16:58:23 +0200

    Add legal_move

--- src/othello/Othello.py
+++ src/othello/Othello.py
@@ -1,6 +1,6 @@
 import Player
 import Board
-import Pawn
+
 class Othello:
 
     def __init__(self):
@@ -48,7 +48,35 @@ class Othello:
         self.get_winner()
 
 
-    def legal_moves(self):
-        pass
+
+    def legal_moves(self,requested_move,check_start=True): # Recurssive approach
+        direction = [(-1, 0), (1, 0), (0, -1), (0, 1),(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        potential_move = []
+        if check_start:
+            check_start=False
+            row,col = requested_move[0], requested_move[1]
+            board , id_player = self.board , self.players_turn
+            id_opponent = (id_player+1)%2
+            for r,c in direction: # This loop determines all the possible moves to explore for 1st neighbors
+                neighbor_r = row + r
+                neighbor_c = col + r
+                if neighbor_r>0 and neighbor_c >0 and neighbor_r<7 and neighbor_c<7:
+                    if not(self.board[neighbor_r,neighbor_c].is_empty):
+                        if self.board[neighbor_r,neighbor_c].pawn.color == id_opponent:
+                            potential_move.append([(neighbor_r,neighbor_c),(r,c)])
+        else:
+            if not(potential_move):
+                return False
+            else:
+                for neighbor_r,neighbor_c,r,c in requested_move:
+                    if neighbor_r>0 and neighbor_c >0 and neighbor_r<7 and neighbor_c<7:
+                        if not(self.board[neighbor_r,neighbor_c].is_empty):
+                            if self.board[neighbor_r,neighbor_c].pawn.color == id_player:
+                                return True
+                            else:
+                                potential_move.append([(neighbor_r,neighbor_c),(r,c)])  
+        return self.legal_moves(potential_move,check_start)
+
+
 
     
