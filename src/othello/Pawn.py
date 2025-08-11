6968e84a50fdf6e7825cea7e07c9f8119d5f9217
Yiran Lu <yiranls@outlook.com>
2025-08-11 16:03:27 +0200

    update Pawn.py

--- src/othello/Pawn.py
+++ src/othello/Pawn.py
@@ -1,2 +1,18 @@
 class Pawn:
-    pass
+    def __init__(self, color: int):
+        """create a pawn of `color`.
+
+        Args:
+            color (int): 0 for black pawn, 1 for white pawn
+        """
+        self._color = color
+
+    @property
+    def color(self):
+        return self._color
+
+    def flip(self):
+        if self._color == 0:
+            self._color = 1
+        else:
+            self._color = 0
