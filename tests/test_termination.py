345c5476cf4e4cc377f4f4e561776b8e34c3cc3e
Yiran Lu <yiranls@outlook.com>
2025-08-12 13:51:38 +0200

    add test_termination.py

---
+++ tests/test_termination.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+board_arr = [
+    [ 1, -1,  0,  0,  0,  0,  0,  0],
+    [ 1,  1,  0,  0,  0,  0,  0,  0],
+    [ 1,  1,  1,  0,  0,  0,  0,  0],
+    [ 1,  1,  0,  1,  0,  0,  0,  0],
+    [ 1,  1,  1,  0,  1,  0,  0,  0],
+    [ 1,  1,  0,  1,  0,  1,  0,  0],
+    [ 1,  1,  0,  0,  1,  0,  1,  0],
+    [ 1,  1,  1,  1,  1,  1,  1,  1],
+]
+
+from othello.Othello import Othello
+from othello.HumanPlayer import HumanPlayer
+from othello.Board import Board
+from othello.Pawn import Pawn
+
+board = Board()
+for i in range(8):
+    for j in range(8):
+        if board_arr[i][j] != -1:
+            board[i,j].pawn = Pawn(board_arr[i][j])
+
+board.display()
+
+player1 = HumanPlayer(0, "Alexis")
+player2 = HumanPlayer(1, "Alicia")
+game = Othello([player1, player2])
+
+print(game.is_terminated())
