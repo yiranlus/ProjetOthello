5f7d85aba658b72845def9ae7c6cf227d067dffe
Mehdi Chouchane <mehdi.chouchan@gmail.com>
2025-08-12 10:18:59 +0200

    Update is_terminated, Add get_possible_move

--- src/othello/Othello.py
+++ src/othello/Othello.py
@@ -1,6 +1,6 @@
 import Player
 import Board
-
+import numpy as np
 class Othello:
 
     def __init__(self):
@@ -14,17 +14,55 @@ class Othello:
         self.board = Board()
         self.players = [Player(0),Player(1)]
 
+
+    def get_possible_moves(self,id_player):
+        """ Return all the possible moves, defined by case which are empty and next to 
+        an opponent's pawn.
+        Args:
+            id_player (int): Player color Id, 0 if black, 1 if white.
+        Output:
+            list of list [x,y] with the coordinates of possible moves for the player
+        """
+        id_opponent= (id_player +1)%2 
+        direction = [(-1, 0), (1, 0), (0, -1), (0, 1),(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        current_board = np.zeros((8,8),int) -1
+        for row in range(8):# Check all the case of the board
+            for col in range(8):
+                if not(self.board[row,col].is_empty): # if there is a pawn on the case
+                    current_board[row,col] =  self.board[row,col].pawn.color # Assign color id to current_board
+        opponent_pos = np.argwhere(current_board==id_opponent) # Get psoition of opponent's pawns
+        possible_move = []
+        for opps in opponent_pos: # Get all the neighbors of the opponent's pawns
+            possible_move+=[[opps[0]+r,opps[1]+c] for r,c in direction ]
+        possible_move = [move for move in possible_move if current_board[move]==-1] # Filter only the empty case
+        return possible_move
+    
     def is_terminated(self):   # WIP
         """ Check if the game is done (no more pawns or legal moves for both players).
         Output:
             True if the game is done, False otherwise
         """     
-        if self.number_pawns>0:
-            if self.legal_moves(0) * self.legal_moves(1) == False: #No legal move for both players
+        direction = [(-1, 0), (1, 0), (0, -1), (0, 1),(-1, -1), (-1, 1), (1, -1), (1, 1)]
+        if self.number_pawns>0: # If there are still pawns to be played
+            current_board = np.zeros((8,8),int) -1
+            
+            possible_move_black = self.get_possible_moves(0)
+            possible_move_white = self.get_possible_moves(1)
+            legal_move_white = False
+            for move in possible_move_white: # Check the legality of all white moves
+                if self.legal_moves(move,id_player=1): 
+                    legal_move_white = True # If legal set True
+                    break
+            legal_move_black = False
+            for move in possible_move_black: # Check the legality of all black moves
+                if self.legal_moves(moveid_player=0):
+                    legal_move_black = True # If legal set True and stop the loop
+                    break
+            if legal_move_black * legal_move_white == False: #No legal move for both players
                 return True
-            else:
+            else: # If there are legal moves to be played by at least one player
                 return False
-        else:
+        else: # If there is no more pawns remaining off the board 
             return True
     
     def get_winner(self):
@@ -68,13 +106,14 @@ class Othello:
 
 
 
-    def legal_moves(self,requested_move,check_start=True): # Recurssive approach
+    def legal_moves(self,requested_move,check_start=True,id_player=None): # Recurssive approach
         direction = [(-1, 0), (1, 0), (0, -1), (0, 1),(-1, -1), (-1, 1), (1, -1), (1, 1)]
         potential_move = []
+        if id_player == None:
+            id_player =  self.players_turn # Get whose turn it is
         if check_start: # if block for the first iteration 
             check_start=False # not going through this block in future runs
             row,col = requested_move[0], requested_move[1]
-            id_player =  self.players_turn # Get whose turn it is
             id_opponent = (id_player+1)%2
             for r,c in direction: # This loop determines all the possible moves to explore for 1st neighbors
                 neighbor_r = row + r
