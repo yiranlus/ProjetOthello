767a2d25285897c8fb2f9b3f5ebc5b70c506240d
Alicia Clark <clarka34@users.noreply.github.com>
2025-08-12 10:09:28 +0200

    updating board display functionality

--- src/othello/Board.py
+++ src/othello/Board.py
@@ -27,88 +27,86 @@ class Board:
         self.board[r, c].pawn = Pawn(color)
 
     
-    def display(self):
-        f = lambda x: x.pawn.color if x.pawn else (-1)
-        f_vec = np.vectorize(f)
+    def display(self, display_choice = 'console'):
+        if display_choice == 'console':
+            f = lambda x: x.pawn.color if x.pawn else np.nan
+            f_vec = np.vectorize(f)
 
-        print(f_vec(self.board))
+            print(f_vec(self.board))
     
+        if display_choice == 'matplotlib':
 
-    def display_graphic(self):
-        x_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-        locs = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
+            x_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+            locs = [0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
         
-        # create 8 x 8 grid of all the indices
-        nx, ny = (7, 7)
-        x = np.linspace(0,nxts(),nx+1, dtype=int)
-        y = np.linspace(0,ny,ny+1, dtype=int)
-        rows, cols = np.meshgrid(x, y, indexing='ij')
+            f2 = lambda x: x.pawn.color if x.pawn else np.nan
+            f_vec2 = np.vectorize(f2)
 
-        ax.scatter(rows + 0.5, cols + 0.5,
-                   s=500,
-                   c = self.board[rows,cols],
-                   cmap=mpl.cm.Greys_r,
-                   edgecolors='black')
+            # create 8 x 8 grid of all the indices
+            nx, ny = (7, 7)
+            x = np.linspace(0,nx,nx+1, dtype=int)
+            y = np.linspace(0,ny,ny+1, dtype=int)
+            rows, cols = np.meshgrid(x, y, indexing='ij')
 
-        # plot on the figure and adjust the axis labels
-        fig, ax = plt.subplots()
-        ax.scatter(rows + 0.5, cols + 0.5,
-                   s=500,
-                   c = self.board[rows,cols],
-                   cmap=mpl.cm.Greys_r,
-                   edgecolors='black')
+            # plot on the figure and adjust the axis labels
+            fig, ax = plt.subplots()
+            ax.scatter(rows + 0.5, cols + 0.5,
+                       s=500,
+                       c = f_vec2(self.board)[rows,cols],
+                       cmap=mpl.cm.Greys_r,
+                       edgecolors='black')
 
-        ax.set_xlim([0,8])
-        ax.set_ylim([0,8])
+            ax.set_xlim([0,8])
+            ax.set_ylim([0,8])
         
-        # remove the major ticks and their corresponding labels in x-dir
-        ax.tick_params(
-            axis='x',          # changes apply to the x-axis
-            which='major',      # both major and minor ticks are affected
-            bottom=False,      # ticks along the bottom edge are off
-            top=False,         # ticks along the top edge are off
-            labelbottom=False) # labels along the bottom edge are off
+            # remove the major ticks and their corresponding labels in x-dir
+            ax.tick_params(
+                axis='x',          # changes apply to the x-axis
+                which='major',      # both major and minor ticks are affected
+                bottom=False,      # ticks along the bottom edge are off
+                top=False,         # ticks along the top edge are off
+                labelbottom=False) # labels along the bottom edge are off
         
-        # remove the minor ticks but keep their corresponding labels in x-dir
-        ax.tick_params(
-            axis='x',          # changes apply to the x-axis
-            which='minor',      # both major and minor ticks are affected
-            bottom=False,      # ticks along the bottom edge are off
-            top=False,         # ticks along the top edge are off
-            labeltop=True,
-            labelbottom=False) # labels along the bottom edge are off
+            # remove the minor ticks but keep their corresponding labels in x-dir
+            ax.tick_params(
+                axis='x',          # changes apply to the x-axis
+                which='minor',      # both major and minor ticks are affected
+                bottom=False,      # ticks along the bottom edge are off
+                top=False,         # ticks along the top edge are off
+                labeltop=True,
+                labelbottom=False) # labels along the bottom edge are off
         
-        # remove the major ticks and their corresponding labels in y-dir
-        ax.tick_params(
-            axis='y',          # changes apply to the x-axis
-            which='major',      # both major and minor ticks are affected
-            left=False,      # ticks along the bottom edge are off
-            right=False,         # ticks along the top edge are off
-            labelleft=False) # labels along the bottom edge are off
+            # remove the major ticks and their corresponding labels in y-dir
+            ax.tick_params(
+                axis='y',          # changes apply to the x-axis
+                which='major',      # both major and minor ticks are affected
+                left=False,      # ticks along the bottom edge are off
+                right=False,         # ticks along the top edge are off
+                labelleft=False) # labels along the bottom edge are off
         
-        # remove the minor ticks but keep their corresponding labels in y-dir
-        ax.tick_params(
-            axis='y',          # changes apply to the x-axis
-            which='minor',      # both major and minor ticks are affected
-            left=False,      # ticks along the bottom edge are off
-            right=False,         # ticks along the top edge are off
-            labelleft=True) # labels along the bottom edge are off
+            # remove the minor ticks but keep their corresponding labels in y-dir
+            ax.tick_params(
+                axis='y',          # changes apply to the x-axis
+                which='minor',      # both major and minor ticks are affected
+                left=False,      # ticks along the bottom edge are off
+                right=False,         # ticks along the top edge are off
+                labelleft=True) # labels along the bottom edge are off
 
-        ax.set_xticks(ticks=locs, labels=x_labels, minor=True)
-        ax.set_yticks(ticks=locs, labels=range(1,9), minor=True)
+            ax.set_xticks(ticks=locs, labels=x_labels, minor=True)
+            ax.set_yticks(ticks=locs, labels=range(1,9), minor=True)
     
-        ax.grid(color='black', linestyle='-', linewidth=1)
-        #ax.set_facecolor('#E1F8DC')
-        ax.set_facecolor('#117950')
-        ax.yaxis.set_inverted(True)
-        
-        plt.show()
+            ax.grid(color='black', linestyle='-', linewidth=1)
+            #ax.set_facecolor('#E1F8DC')
+            ax.set_facecolor('#117950')
+            ax.yaxis.set_inverted(True)
+            
+            plt.show()
 
 
 if __name__ == "__main__":
     board = Board()
     board.place_pawn(3, 2, 1)
     board.place_pawn(1,1,1)
-    print(board.display_console())
-    #print(board.display())
+    print(board.display())
+    print(board.display_graphic())
     
