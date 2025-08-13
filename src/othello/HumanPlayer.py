dd2e382ccd087e862da8a94ff892e1bf4dc633df
Yiran Lu <yiranls@outlook.com>
2025-08-13 12:20:04 +0200

    minor fix

--- src/othello/HumanPlayer.py
+++ src/othello/HumanPlayer.py
@@ -14,11 +14,11 @@ class HumanPlayer(Player):
         while not is_good_coord:
             try:
                 coord = input(f"Your movement ({self.name}, {self.color} {circle}): ")
-                if len(coord) != 2:
-                    raise ValueError()
                 if coord == "q":
                     print("Game terminated.")
                     exit(0)
+                if len(coord) != 2:
+                    raise ValueError()
                 if ord("A") <= ord(coord[0]) <= ord("H"):
                     c = ord(coord[0]) - ord("A")
                 elif ord("a") <= ord(coord[0]) <= ord("h"):
